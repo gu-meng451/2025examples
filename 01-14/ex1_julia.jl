64ac697ee5e67088cf4daa18d18544d130eb573d
tim <fitzgeraldt@gonzaga.edu>
2025-01-14 14:33:59 -0800

    This is an example change

--- 01-14/ex1_julia.jl
+++ 01-14/ex1_julia.jl
@@ -7,3 +7,5 @@ c = 0.1 # a Float64
 @printf("a = %.16f, type(a) = %s\n", a, typeof(a))
 @printf("b = %.16f, type(b) = %s\n", b, typeof(b))
 @printf("c = %.16f, type(c) = %s\n", c, typeof(c))
+
+# This is a change
