e5fe9144cfd188ad81a757b3feb12d89f0f00c98
tim <fitzgeraldt@gonzaga.edu>
2025-01-14 17:10:17 -0800

    Add comments

--- 01-14/ex1_python.py
+++ 01-14/ex1_python.py
@@ -2,10 +2,12 @@
 
 if __name__ == '__main__':
 
+    # define variables
     a = 1//10
     b = 1/10
     c = 0.1
 
+    # print output
     print( 'a = {0:.16f}'.format(a) )
     print( 'b = {0:.16f}'.format(b) )
     print( 'c = {0:.16f}'.format(c) )
