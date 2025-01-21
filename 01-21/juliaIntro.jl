f15c820cc5bb27e7a7d2013b9a1481b549586749
tim <fitzgeraldt@gonzaga.edu>
2025-01-21 09:22:08 -0800

    Julia Examples

---
+++ 01-21/juliaIntro.jl
@@ -0,0 +1,25 @@
+using Printf
+
+## Strings
+
+
+## Dictionaries
+# key => value
+
+
+## Strucs
+
+
+## Memory Scope
+# for loops and simple functions
+
+# for loops:
+
+
+# list Comprehension
+
+
+## while loops
+
+
+## Modules (groupings of functions)
