beac8dbb45898b3b860dd2f961b06a37b1dc7219
tim <fitzgeraldt@gonzaga.edu>
2025-01-21 16:47:20 -0800

    as modified in class

--- 01-21/myexp.m
+++ 01-21/myexp.m
@@ -3,13 +3,19 @@ close all
 clc
 
 %%
-x = 1;
 
 %%
+x = -10;
 
+[y1, iter1] = MyExp(x)
+
+abs(exp(x) - y1)
+
+%%
+function [my_exp, iter] = MyExp(x)
 iter = 0;
 maxiter = 1000;
-tol = 1e-6;
+tol = 1e-8;
 flag = 0;
 my_exp = 1;
 newterm = 1;
@@ -20,6 +26,18 @@ while flag == 0
     newterm = newterm*x/iter;
     my_exp = my_exp + newterm;
 
-    % this does not end yet
+    % absolute error:
+    % abs_err = abs( exp(1) - my_exp );
+
+    % rel_err
+    err = abs( newterm );
+
+    if iter >= maxiter
+        flag = -1;
+        error("Failed to converge in %d steps", iter);
+    elseif err < tol
+        flag = 1;
+    end
 end
 
+end
