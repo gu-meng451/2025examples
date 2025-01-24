b4c40fbc426506fb49e8722bf0305a9cd103286f
tim <fitzgeraldt@gonzaga.edu>
2025-01-23 17:35:55 -0800

    Live from class

--- 01-23/juliaIntro.jl
+++ 01-23/juliaIntro.jl
@@ -1,13 +1,28 @@
+# using Pkg
+# Pkg.activate(".")
+# using Plots
+
 using Printf
+# using LinearAlgebra
 
 ## Strings
-
+a = "this is a string"
 
 ## Dictionaries
 # key => value
+A = Dict( 1 => "value1", "2"=>rand(), :hamburger => t-> cos(t) )
 
+# cos(1)
+A[:hamburger](1)
 
 ## Strucs
+mutable struct myCustomStruct
+    prop1
+    prop2::String
+    prop3::Int
+end
+
+B = myCustomStruct(pi, "This is a String", 2)
 
 
 ## Memory Scope
